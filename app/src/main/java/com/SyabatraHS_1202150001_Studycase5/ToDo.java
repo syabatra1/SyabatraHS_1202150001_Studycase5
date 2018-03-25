f014a466e7001c8c3faae5c613aa9a2460d6cd8e
wildanaditya <wildanaditya@365.telkomuniversity.ac.id>
2018-03-25 17:31:02 +0700

    SyabatraHS_1202150001_Studycase5

---
+++ app/src/main/java/com/SyabatraHS_1202150001_Studycase5/ToDo.java
@@ -0,0 +1,58 @@
+package com.SyabatraHS_1202150001_Studycase5;
+
+
+public class ToDo {
+    private int id;
+    private String name;
+    private String desc;
+    private String priority;
+
+    public ToDo(){
+
+    }
+
+    public ToDo(String name, String desc, String priority){
+        this.name = name;
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public ToDo(int id, String name, String desc, String priority){
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+}
